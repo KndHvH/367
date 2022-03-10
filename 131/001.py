80117301a966d7aafe155ca5d06721542af0099b
KndHvH <delend89@gmail.com>
2022-03-10 09:42:20 -0300

    1

--- 131/001.py
+++ 131/001.py
@@ -27,38 +27,42 @@ x=[]
 for i in range(0,10,1):
     x.append(input())
 
-print(x[0])
+#print(x[0])
     #x[0]='z'
 for i in range(0,10,1):
     x.append(x[i].upper())
 
-print("T",z[2],x[0],x[5])
-for i in range(0,19,1):
+#print("T",z[2],x[0],x[5])
+for i in range(0,len(z),1):
 
-    for l in range(0,len(z),1):
+    for l in range(0,19,1):
 
-        if z[l]==x[i]:
+        if z[i]==x[l]:
             #  z[2]==x[0]
             #  'z'=='z'
-            if i < 10:
-                if i <5:
-                    print((z[l]) + "->" + (x[i + 5]))
-                    z[l]=x[i+5]
+            if l < 10:
+                if l <5:
+                #    print((z[i]) + "->" + (x[l + 5]))
+                    z[i]=x[l+5]
+                    break
                 #   z[2]=x[0+5]
                 #   z[2]=x[5]='p'
                 else:
-                    print((z[l])+"->"+(x[i-5]))
-                    z[l]=x[i-5]
+                #    print((z[i])+"->"+(x[l-5]))
+                    z[i]=x[l-5]
+                    break
             else:
-                if i <15:
-                    print((z[l])+"->"+(x[i+5]))
-                    z[l]=x[i+5]
+                if l <15:
+                #    print((z[i])+"->"+(x[l+5]))
+                    z[i]=x[l+5]
+                    break
 
                 else:
-                    print((z[l])+"->"+(x[i-5]))
-                    z[l]=x[i-5]
+                #    print((z[i])+"->"+(x[l-5]))
+                    z[i]=x[l-5]
+                    break
 
-print("T",z[2],x[0],x[5])
+#print("T",z[2],x[0],x[5])
 print(*z)
 
 
