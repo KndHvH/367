62c13faf73f644ae875eb0b2fe053cf096b0a5f0
Knd <delend89@gmail.com>
2022-03-10 15:07:40 -0300

    1

--- 161/api.py
+++ 161/api.py
@@ -3,19 +3,28 @@
 -flask
 
 '''
+import gcloud
 import pandas as pd
 from flask import Flask
 
+gcloud.init
 
 app = Flask(__name__)
 
 #funcionalidades
+@app.route('/')
 def homepage():
     return 'Essa é a home page do site'
 
+@app.route('/contatos')
+def contatos():
+    return 'Essa é a page de contatos do site'
+
 
 #rodar api
-app.run()
+#app.run()
+gcloud.app.deploy
+
 
 
 
