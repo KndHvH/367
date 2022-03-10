646c4e9a15925e9ffa33f5e6d1a9ce1299920109
logonrmlocal <delend89@gmail.com>
2022-03-09 22:15:43 -0300

    1

--- 131/001.py
+++ 131/001.py
@@ -8,36 +8,57 @@
 2 = 7
 5 = 1
 
+zenit
+polar
+
 '''
 
 z=[
+       #z[2]='z'
     'g','h','z','_','H','I','i','k',
     '0','v','n','k','w','B','u','U',
     'U','Z','n','y','6','e','k','S',
     '3','G','U','V','V','Z','2','k',
     'N','k','3','X','F','k','C','z',
 ]
+
 x=[]
 
 for i in range(0,10,1):
-   x.append(input())
+    x.append(input())
 
+print(x[0])
+    #x[0]='z'
 for i in range(0,10,1):
-   x.append(x[i].upper())
+    x.append(x[i].upper())
 
-for i in range(0,len(z)):
-    for l in range(0,len(x)):
+print("T",z[2],x[0],x[5])
+for i in range(0,len(z),1):
+
+    for l in range(0,19,1):
+
         if z[i]==x[l]:
+            #  z[2]==x[0]
+            #  'z'=='z'
             if l < 10:
                 if l <5:
+                    print((z[i]) + "->" + (x[l + 5]))
                     z[i]=x[l+5]
+                #   z[2]=x[0+5]
+                #   z[2]=x[5]='p'
                 else:
+                    print((z[i])+"->"+(x[l-5]))
                     z[i]=x[l-5]
             else:
                 if l <15:
+                    print((z[i])+"->"+(x[l+5]))
                     z[i]=x[l+5]
+
                 else:
+                    print((z[i])+"->"+(x[l-5]))
                     z[i]=x[l-5]
+
+print("T",z[2],x[0],x[5])
 print(*z)
 
 
--- 131/test.py
+++ 131/test.py
@@ -38,10 +38,26 @@ for i in g1:
 
 
 
-a = ("b aa nn aa")
+'''a = ("b aa nn aa")
 
 print(a)
 
 print(a.replace(" ", ""))
+'''
+'''a=[1,2,3]
 
+for i in range(0,3,1):
+    for j in range(0,3,1):
+        print(a[i],a[j])
+'''    # 0  1  2
+'''letras=[ a, b, c,]
+print(letras[1])'''
+'''nomes= []
+for i in range(0,3,1):
+    #nomes.append(input("Digite o nome: "))
+    print(i)
+#print(nomes)'''
 
+a=[10,20,30,40]
+
+print(a[0]+a[1+1])
