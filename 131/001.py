21eb3821a887c9a25e3cbbb14639508e8011edb6
Knd <delend89@gmail.com>
2022-03-10 10:51:57 -0300

    1

--- 131/001.py
+++ 131/001.py
@@ -8,13 +8,11 @@
 2 = 7
 5 = 1
 
-zenit
-polar
 
 '''
 
 z=[
-       #z[2]='z'
+
     'g','h','z','_','H','I','i','k',
     '0','v','n','k','w','B','u','U',
     'U','Z','n','y','6','e','k','S',
@@ -27,44 +25,44 @@ x=[]
 for i in range(0,10,1):
     x.append(input())
 
-#print(x[0])
-    #x[0]='z'
+
 for i in range(0,10,1):
     x.append(x[i].upper())
 
-#print("T",z[2],x[0],x[5])
+
 for i in range(0,len(z),1):
 
     for l in range(0,19,1):
 
         if z[i]==x[l]:
-            #  z[2]==x[0]
-            #  'z'=='z'
+
             if l < 10:
                 if l <5:
-                #    print((z[i]) + "->" + (x[l + 5]))
+
                     z[i]=x[l+5]
                     break
-                #   z[2]=x[0+5]
-                #   z[2]=x[5]='p'
+
                 else:
-                #    print((z[i])+"->"+(x[l-5]))
+
                     z[i]=x[l-5]
                     break
             else:
                 if l <15:
-                #    print((z[i])+"->"+(x[l+5]))
+
                     z[i]=x[l+5]
                     break
 
                 else:
-                #    print((z[i])+"->"+(x[l-5]))
+
                     z[i]=x[l-5]
                     break
 
-#print("T",z[2],x[0],x[5])
+
 print(*z)
 
+y=str(z)
+print(z.replace(" ", ""))
 
 
-#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
+
+
