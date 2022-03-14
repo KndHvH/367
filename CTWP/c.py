b9ac8c82a3f41f1015c5d3dfeff252d3e74d1b72
KndHvH <delend89@gmail.com>
2022-03-14 00:05:04 -0300

    1

---
+++ CTWP/c.py
@@ -0,0 +1,12 @@
+#5
+
+distancia=int(input("Digite a distancia da viagem em km: "))
+vmedia=int(input("Digite a velocidade media prevista em km/h: "))
+
+tempo=distancia/vmedia
+
+print(f"Tempo previsto de viagem: {tempo} horas.")
+
+
+print("_______________________")
+#6
