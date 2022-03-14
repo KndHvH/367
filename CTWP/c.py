8263378711a8958b098c89e073921160d5c03435
KndHvH <delend89@gmail.com>
2022-03-14 00:26:15 -0300

    1

--- CTWP/c.py
+++ CTWP/c.py
@@ -1,4 +1,4 @@
-#5
+#6
 
 distancia=int(input("Digite a distancia da viagem em km: "))
 vmedia=int(input("Digite a velocidade media prevista em km/h: "))
@@ -9,4 +9,24 @@ print(f"Tempo previsto de viagem: {tempo} horas.")
 
 
 print("_______________________")
-#6
+#7
+
+c=int(input("Digite a temperatura e celsius: "))
+
+f = float(c*1.8 + 32)
+
+print(f"Temperatura em Fahrenheit: {f}")
+
+
+print("_______________________")
+#8
+
+km=int(input("Digite a qnt de KM percorrido "))
+dias=int(input("Digite a qnt de dias alugado "))
+
+val_km=km*0.15
+val_dias=dias*60
+
+total=val_km+val_dias
+
+print("Total gasto: R$",total)
