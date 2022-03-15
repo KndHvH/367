a74d8d2c68d8259173d0c3e1046894f2bca58bad
logonrmlocal <delend89@gmail.com>
2022-03-14 22:53:54 -0300

    1

---
+++ RE/d.r
@@ -0,0 +1,11 @@
+> nomes <- c("Marcos de Aguiar","Paula Miranda","Emilio Santos","Priscila de Albuquerque")
+>
+> nomes[4] <- c("Prycilla de Albuquerque")
+>
+> salario <- c(4000,2500,1200,3400)
+>
+> salarioliquido <- salario*0.70
+> salarioliquido
+[1] 2800 1750  840 2380
+
+
