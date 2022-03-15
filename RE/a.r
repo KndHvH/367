bab20d9f966833997a2b5986f2201b9869ad8509
logonrmlocal <delend89@gmail.com>
2022-03-14 22:14:37 -0300

    1

---
+++ RE/a.r
@@ -0,0 +1,35 @@
+#citaçõpes e demonstrações
+citation()
+demo(persp)
+
+#personalizando o R
+#Tools -> Global Options -> Appearance -> drakula
+
+
+#R como calculadora
+4+6
+7-9
+4/2
+4%%2
+4*3
+2*3
+2**3
+
+log(1)
+factorial(5)
+abs(-100)
+sin(30)
+
+
+#VETORES
+
+
+x<-c(3,4,5)
+x
+
+vendas <- c(12,15,3,5)
+meses <- c("Jan","Fev","Mar","Abr")
+
+
+vendas2 <- scan()
+vendas2
---
+++ RE/b.r
@@ -0,0 +1,22 @@
+> log(1)
+[1] 0
+> factorial(5)
+[1] 120
+> abs(-100)
+[1] 100
+> sin(30)
+[1] -0.9880316
+> sin(pi/6)
+[1]  0.5
+
+x<-c(3,4,5)
+#    1 2 3
+
+vendas <- c(12,15,3,5)
+meses <- c("Jan","Fev","Mar","Abr")
+
+vendas2 <- scan()
+#  input
+
+> vendas[1]
+[1] 12
