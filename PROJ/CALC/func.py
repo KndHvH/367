d473d8f003e12e9f67ad0aafb4b176eb169b8e7f
Knd <delend89@gmail.com>
2022-03-21 11:08:13 -0300

    1

---
+++ PROJ/CALC/func.py
@@ -0,0 +1,24 @@
+def operacao():
+    return input("o que deseja realizar?\n" +
+        "<S> - Para Somar 2 numeros\n" +
+        "<U> - Para Subtrair 2 numeros\n" +
+        "<D> - Para Dividir 2 numeros\n" +
+        "<M> - Para Multiplicar 2 numeros\n" +
+        "<E> - Para Sair do programa: "\
+         ).upper()
+
+def verif():
+    x = 1
+    while x == 1:
+        ope = operacao()
+        if ope == 'S' or ope == 'U' or ope == 'D' or ope == 'M' or ope == 'E':
+            x = 0
+        else:
+            print("Operação invalida!")
+            x = 1
+
+
+def conta():
+    if ope == 'S':
+        resultado = n1 + n2
+    elif ope == 'U'
---
+++ PROJ/CALC/main.py
@@ -0,0 +1,8 @@
+from func import *
+resultado=0
+
+n1=int(input("Digite o primeiro numero: "))
+
+verif()
+
+n2=int(input("Digite o proximo numero: "))
