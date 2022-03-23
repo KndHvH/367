ef057fb64538ae525db5ffff0b5720a6b8ebf650
Knd <delend89@gmail.com>
2022-03-23 11:24:33 -0300

    1

---
+++ FIAP_NANO/Python/cap4/Dicionarios/Dicionarios3.py
@@ -0,0 +1,15 @@
+usuarios={
+    "chaves":["Chaves Silva","17/06/2017","Recep_01"],
+    "quico":["Enrico Flores","03/06/2017","Raiox_02"],
+    "matias":["Matias Herklotz","02/05/2002","Atend_03"],
+}
+
+
+
+print(usuarios.items())
+print("\n---\n")
+print(usuarios.values())
+print("\n---\n")
+print(usuarios.keys())
+print("\n---\n")
+print(usuarios.has_key())
