87e8894d1316cf2444675c898ae735c14a3d16b4
Knd <delend89@gmail.com>
2022-03-28 13:25:23 -0300

    1

---
+++ FIAP_NANO/Python/cap4/Dicionarios/Metodos adicionais.py
@@ -0,0 +1,19 @@
+usuarios={
+    "001":["Silva","2017"],
+    "002":["Flores","2017"],
+    "003":["Matias","2002"],
+}
+
+
+
+print('usuarios.items()')
+print(usuarios.items())
+
+print('\n\nusuarios.values()')
+print(usuarios.values())
+
+print('\n\nusuarios.keys()')
+print(usuarios.keys())
+
+'''print('\n\nusuarios.has_key()')
+print(usuarios.has_key('1'))'''
--- FIAP_NANO/Python/cap4/Tuplas/tupla.py
+++ FIAP_NANO/Python/cap4/Tuplas/tupla.py
@@ -1,8 +1,26 @@
+#dicionario
 usuarios = {}
 
+#lista
 emails = ["abc@gmail.com","def@gmail.com"]
 
+#tupla = lista de (0,1,2... + lista de emails)
 tupla = list(enumerate(emails))
+#[(0, 'abc@gmail.com'), (1, 'def@gmail.com')]
 
+# for i dentro do tamanho de tupla, no caso 2
 for i in range(0, len(tupla)):
+
+    #print tupla [0/1] posição [1]
     print("Email: ", tupla[i][1])
+
+    #dicionario [tupla[i]] -> dicionario[0/1] = [input,input2]
+    usuarios[tupla[i]] = [input("Digite o nome: "),input("Digite o nivel: ")]
+    #{(0, 'abc@gmail.com'): ['abece', '33'], (1, 'def@gmail.com'): ['deefee', '44']}
+
+print(usuarios)
+for i, dado in usuarios.items():
+    print("Usuario.:",i[0])
+    print("Email...:",i[1])
+    print("Nome....:",dado[0])
+    print("Nivel...:",dado[1])
