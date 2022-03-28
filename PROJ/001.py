6ada7d34ec087706050d7f73b0cf98eba333dc26
logonrmlocal <dd>
2022-03-28 18:59:24 -0300

    1

--- PROJ/001.py
+++ PROJ/001.py
@@ -1,18 +1,18 @@
 
 z=[
-    'g','h','z','_','G','c','W','P',
-    'q','8','9','H','P','g','m','C',
-    '3','r','W','g','x','k','o','r',
-    'H','T','J','y','I','Y','G','i',
-    '9','L','1','1','e','0','6','m',
+    'g','h','z','_','G','c','W','P','q','8','9','H','P','g','m','C','3','r','W','g','x','k','o','r','H','T','J','y','I','Y','G','i','9','L','1','1','e','0','6','m',
 ]
 x=[]
-for i in range(0,10,1):
-    x.append(input())
-for i in range(0,10,1):
+senha=input()
+x=list(senha)
+
+
+for i in range(0,10):
     x.append(x[i].upper())
+
+
 for i in range(0,len(z),1):
-    for l in range(0,19,1):
+    for l in range(0,19):
         if z[i]==x[l]:
             if l < 10:
                 if l <5:
