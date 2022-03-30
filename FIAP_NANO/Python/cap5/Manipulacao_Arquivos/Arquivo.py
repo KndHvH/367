2515dab07165b9b1db9ab7b1af7a9a05f7d52b3f
Knd <delend89@gmail.com>
2022-03-30 14:17:10 -0300

    sinapse

--- FIAP_NANO/Python/cap5/Manipulacao_Arquivos/Arquivo.py
+++ FIAP_NANO/Python/cap5/Manipulacao_Arquivos/Arquivo.py
@@ -4,7 +4,5 @@ arquivo.write("Meu primeiro arquivo, ai que festa!")
 
 arquivo.close()'''
 
-x=1
-while x==1:
-    with open("arquivo1.txt","a") as arquivo:
-        arquivo.write("\nHakuna Matata!!")
+with open("arquivo1.txt","a") as arquivo:
+    arquivo.write("\nHakuna Matata!!")
