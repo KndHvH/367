46025bea8a77f6ea647500b6e5d74a423e5e57f7
logonrmlocal <ss>
2022-04-11 20:52:08 -0300

    fiap

---
+++ FIAP/Python/aula/aula2.0.py
@@ -0,0 +1,13 @@
+a=int(input("Digite um numero: "))
+b=int(input("Digite um numero: "))
+c=int(input("Digite um numero: "))
+
+maior = a
+
+if b > maior:
+    maior = b
+
+if c > maior:
+    maior  = c
+
+print(maior)
