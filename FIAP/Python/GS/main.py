bce7743fa3d5cfdc93a259cd7990be5c2d6d5a8e
KndHvH <delend89@gmail.com>
2022-05-30 20:28:28 -0300

    ml

--- FIAP/Python/GS/main.py
+++ FIAP/Python/GS/main.py
@@ -1,31 +1,79 @@
 
-print("VOTAÇÃO - SEGUNDO TURNO")
 
+
+
+
+print("\033[91mVOTAÇÃO - SEGUNDO TURNO\033[0m")
+
+legenda = ['Nulo','Blue','Red']
 decisao = 1
-voto = 1
 listaVotos = []
+tamanhoLista = 0
+votosNulo = 0
+votosBlue = 0
+votosRed = 0
+totalVotos = 0
 
 while decisao != 0:
-    print("-----------------------")
-    print("1 – Efetuar a votação")
-    print("2 – Exibir o Relatório dos Votos")
-    print("3 – Efetuar a apuração | Exibir o resultado")
-    print("4 – Mostrar o percentual dos votos")
+    print("\033[1m-----------------------\033[0m")
+    print("\033[92m1\033[0m – Efetuar a votação")
+    print("\033[92m2\033[0m – Exibir o Relatório dos Votos")
+    print("\033[92m3\033[0m – Efetuar a apuração | Exibir o resultado")
+    print("\033[92m4\033[0m – Mostrar o percentual dos votos")
 
-    decisao = int(input("Digite a opção desejada:_"))
+    decisao = int(input("\033[92mDigite a opção desejada:_\033[0m"))
 
     if decisao == 1:
+        voto = 1
         while voto != 0:
-            print("-----------------------")
-            print("1 – Votar Candidato: Blue")
-            print("2 – Votar Candidato: Red")
-            print("3 – Voto: Nulo")
-            print("0 – SAIR")
-            voto = int(input("Digite a opção desejada:_"))
+            print("\033[1m-----------------------\033[0m")
+            print("\033[92m1\033[0m – Votar Candidato: Blue")
+            print("\033[92m2\033[0m – Votar Candidato: Red")
+            print("\033[92m3\033[0m – Voto: Nulo")
+            print("\033[92m0\033[0m – SAIR")
+            voto = int(input("\033[92mDigite a opção desejada:_\033[0m"))
             if voto!=0:
                 listaVotos.append(voto)
+                tamanhoLista += 1
+        for i in range(0, tamanhoLista):
+            if listaVotos[i] != 1 and listaVotos[i] != 2:
+                listaVotos[i] = 0
 
-print(listaVotos)
+    if decisao == 2:
+        if listaVotos != []:
+            for i in range(0,tamanhoLista):
+                print(f'Voto da pessoa {i+1} foi {legenda[listaVotos[i]]}')
+        else:
+            print('Nenhum Voto Computado!')
+
+    if decisao == 3:
+        for i in range(0, tamanhoLista):
+                if listaVotos[i] == 0:
+                    votosNulo += 1
+                elif listaVotos[i] == 1:
+                    votosBlue += 1
+                else:
+                    votosRed += 1
+        totalVotos = votosRed+votosBlue+votosNulo
+        print("\033[1m-----------------------\033[0m")
+        print(f'\033[93mTotal de votos Nulos: {votosNulo}\033[0m')
+        print(f'\033[94mTotal de votos Blue: {votosBlue}\033[0m')
+        print(f'\033[91mTotal de votos Red: {votosRed}\033[0m')
+        print(f'\033[92mTotal de votos: {totalVotos}\033[0m')
+
+    if decisao == 4:
+        if listaVotos == []:
+            print('Nenhum Voto Computado!')
+        elif totalVotos == 0:
+            print('Resultado não apurado!')
+        else:
+            print("\033[1m-----------------------\033[0m")
+            print(f'\033[93mTotal de votos Nulos: {votosNulo}, porcentagem: {(votosNulo/totalVotos)*100:.2f}%\033[0m')
+            print(f'\033[94mTotal de votos Blue: {votosBlue}, porcentagem: {(votosBlue/totalVotos)*100:.2f}%\033[0m')
+            print(f'\033[91mTotal de votos Red: {votosRed}, porcentagem: {(votosRed/totalVotos)*100:.2f}%\033[0m')
+            print(f'\033[92mTotal de votos: {totalVotos}, porcentagem: {(totalVotos/totalVotos)*100:.2f}%\033[0m')
+
+
 
 
 
