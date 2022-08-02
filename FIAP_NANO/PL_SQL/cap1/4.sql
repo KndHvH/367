d346050540b189ca346780a0dfe637e5580af16c
KndHvH <delend89@gmail.com>
2022-08-02 14:35:37 -0300

    PL/SQL

--- FIAP_NANO/PL_SQL/cap1/4.sql
+++ FIAP_NANO/PL_SQL/cap1/4.sql
@@ -28,4 +28,53 @@ DBMS_OUTPUT.PUT_LINE(v_job);
 END;
 /
 
+/
+DECLARE
+    v_soma_sal  NUMBER;
+    v_deptno    NUMBER NOT NULL := 20;
+BEGIN
+    SELECT SUM(SAL)
+    INTO v_soma_sal
+    from EMP
+    WHERE deptno = v_deptno;
+DBMS_OUTPUT.PUT_LINE('A soma dos salários do departamento ' ||v_deptno||' é '||v_soma_sal);
+END;
+/
 
+
+
+
+
+
+
+/
+select 'alter system kill session ''' || s.sid || ',' || s.serial# || ',@' || s.inst_id || ''' immediate;' cmd
+ ,      s.username
+ ,      o.object_name
+ ,      s.sid
+ ,      s.serial#
+ ,      s.inst_id
+ ,      p.spid
+ ,      s.program
+ ,      s.machine
+ ,      s.osuser
+ ,      s.port
+ ,      s.logon_time
+ ,      sq.sql_text
+ from   gv$locked_object l
+ ,      dba_objects      o
+ ,      gv$session       s
+ ,      gv$process       p
+ ,      gv$sql           sq
+ where  o.object_id = l.object_id
+ and    s.sid = l.session_id
+ and    s.inst_id = l.inst_id
+ and    p.addr = s.paddr
+ and    sq.address(+) = s.sql_address
+ order by s.username
+ ,      s.inst_id
+ ,      s.sid
+ ,      s.serial#
+ ,      o.object_name;
+
+/
