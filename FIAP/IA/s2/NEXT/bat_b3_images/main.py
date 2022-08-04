2315af2ac950a1896ad526f8563ebba1ad882555
KndHvH <delend89@gmail.com>
2022-08-03 23:46:49 -0300

    next

--- FIAP/IA/s2/NEXT/bat_b3_images/main.py
+++ FIAP/IA/s2/NEXT/bat_b3_images/main.py
@@ -1,9 +1,15 @@
 import boto3
+from dotenv import dotenv_values
+
+ENV_DATA = dotenv_values(".env")
 
 
 client = boto3.client(
     service_name='s3',
-    aws_access_key_id='seu_access_key_id',
-    aws_secret_access_key='seu_secret_access_key',
+    aws_access_key_id=ENV_DATA["AWS_KEY_ID" ],
+    aws_secret_access_key=ENV_DATA["AWS_ACCESS_KEY"],
     region_name='sa-east-1' # voce pode usar qualquer regiao
+
 )
+time = funcaosxa
+client.upload_file("foto.jpg", "albumknd", icaro+time)
