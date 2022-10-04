e07808c82088bb93f680e7c39665fb2116a4bb19
Knd <delend89@gmail.com>
2022-10-03 22:29:17 -0300

    estatistica

---
+++ FIAP/Estatistica/s2/poison/materia.md
@@ -0,0 +1,10 @@
+P(X=x) = (e ^-lam * lam^x) x!
+
+
+media de 5 ligacoes por minuto
+
+qual a chance de 15 ligacoes em 4 minutos?
+
+lambda = 20 = 5/1*4
+
+e = constant
--- FIAP/test.py
+++ FIAP/test.py
@@ -1,11 +1,24 @@
 
 import requests
 
+import json
+
 url = 'http://0.0.0.0:8000/v1/sentiment'
 
 response = requests.get(url)
 
-print(response)
+comment = {
+  "text": "Customer dissatisfied with product delivery deadlines, worried and limited products/volumes for new negotiation., alternative is to migrate to competitors and open doors to new experiences. I shared with him our problems with cost increases and product supply, caused by problems generated due to global market and political movement, as we depend on active ingredients supplied by other countries. The grade 5 in delivery reliability was generated due to delays in deliveries compared to the initial schedule passed by the customer.",
+  "note": 6,
+  "companyId": 1940324,
+  "responseDate": "2022-09-29T23:10:54.640Z"
+}
+
+response = requests.post(url,data=json.dumps(comment))
+
+
+
+print(response.json())
 
 # link = "https://ousadia-api.herokuapp.com/v1/sentiment"
 
