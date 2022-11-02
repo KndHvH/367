8ed7367b4c02c8493ba9c0106d23b627c8ef5777
kndhvh <delend89@gmail.com>
2022-11-01 22:08:02 -0300

    class

--- ALURA/PythonOO/class.py
+++ ALURA/PythonOO/class.py
@@ -5,20 +5,21 @@ class Conta:
 
         print('Contruindo', self)
 
-        self.id = id
-        self.titular = name
-        self.saldo = value
-        self.limite = limit
+        self.__id = id
+        self.__titular = name
+        self.__saldo = value
+        self.__limite = limit
 
     def extract(self):
-        print(f'Conta: {self.id}')
-        print(f'Nome: {self.titular}')
-        print(f'Saldo: R${self.saldo:.2f}')
+        print(f'Conta: {self.__id}')
+        print(f'Nome: {self.__titular}')
+        print(f'Saldo: R${self.__saldo:.2f}')
+        print(f'Limite: R${self.__limite:.2f}')
 
     def deposit(self, value):
 
-        self.saldo += value
+        self.__saldo += value
 
     def withdraw(self, value):
 
-        self.saldo -= value
+        self.__saldo -= value
