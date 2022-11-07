f0da353bfc6aaa3ab0f5833833191816858ffc51
kndhvh <delend89@gmail.com>
2022-11-07 17:18:40 -0300

    oo2

---
+++ ALURA/PythonObjeto2/modelo.py
@@ -0,0 +1,123 @@
+
+class Program:
+
+    info = 'Father class'
+
+    def __init__(self, name, year, likes=0) -> None:
+
+        self.__name = name
+        self.__year = year
+        self.__likes = likes
+
+    def liked(self):
+        self.__likes += 1
+
+    def __str__(self):
+        likes = f'Likes: {self.likes}' if self.likes > 1 else f'Like: {self.likes}'
+        return f'Name: {self.name} \nYear: {self.year}\n{likes}'
+
+    @property
+    def name(self):
+        return self.__name.title()
+
+    @property
+    def year(self):
+        return self.__year
+
+    @property
+    def likes(self):
+        return self.__likes
+
+    @classmethod
+    def info(cls):
+        return cls.info
+
+
+class Film(Program):
+
+    def __init__(self, name, year, duration, likes=0) -> None:
+        super().__init__(name, year, likes)
+
+        self.__duration = duration
+
+    @property
+    def duration(self):
+        return self.__duration
+
+    def __str__(self):
+        likes = f'Likes: {self.likes}' if self.likes > 1 else f'Like: {self.likes}' 
+        return f'Name: {self.name} \nYear: {self.year}\nDuration: {self.duration} min\n{likes}'
+
+
+class Serie(Program):
+
+    def __init__(self, name, year, seasons, likes=0) -> None:
+        super().__init__(name, year, likes)
+
+        self.__seasons = seasons
+
+    @property
+    def seasons(self):
+        return self.__seasons
+
+    def __str__(self):
+        likes = f'Likes: {self.likes}' if self.likes > 1 else f'Like: {self.likes}'
+        seasons = f'{self.seasons} Seasons' if self.seasons > 1 else f'{self.seasons}Season'
+        return f'Name: {self.name} \nYear: {self.year}\nDuration: {seasons}\n{likes}'
+
+
+class Playlist:
+
+    def __init__(self, name, programs) -> None:
+        self.__name = name
+        self.__programs = programs
+        self.__size = self.__getSize()
+
+    def __getSize(self):
+        return len(self.__programs)
+
+    def __str__(self) -> str:
+        return f'Name: {self.name}\nSize: {self.size}'
+
+    def showcase(self):
+        print(self)
+        for i in self.programs:
+            print(10*'--')
+            print(i)
+
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def size(self):
+        return self.__size
+
+    @property
+    def programs(self):
+        return self.__programs
+
+
+vingadores = Film('Vingadores', 2015, 160)
+starwars = Film('star wars', 1999, 130)
+got = Serie('Game of Thrones', 2012, 8)
+dark = Serie('dark', 2019, 3)
+
+
+dark.liked()
+dark.liked()
+dark.liked()
+dark.liked()
+got.liked()
+got.liked()
+starwars.liked()
+starwars.liked()
+starwars.liked()
+vingadores.liked()
+
+
+programs = [vingadores, got, starwars, dark]
+
+favoritos = Playlist('Favorites', programs)
+
+favoritos.showcase()
