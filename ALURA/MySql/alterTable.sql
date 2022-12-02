487b6a5b8a96df39e6db5d1f9fe6f00c0f300882
knd <delend89@gmail.com>
2022-12-02 01:24:11 -0300

    altertable

--- ALURA/MySql/alterTable.sql
+++ ALURA/MySql/alterTable.sql
@@ -1,3 +1,22 @@
 -- Active: 1669499925647@@127.0.0.1@3306@juicedb
 
+UPDATE `tbProducts`
+SET
+    `PACKAGE` = 'Lata',
+    `PRICE` = 2.46
+WHERE `ID` = 544931;
 
+DELETE FROM `tbProducts` WHERE `ID` = 544931;
+
+ALTER TABLE `tbProducts` ADD PRIMARY KEY (ID);
+
+ALTER TABLE `tbProducts` ADD COLUMN  (CREATED TIME);
+
+
+ALTER TABLE `tbProducts` MODIFY CREATED DATE;
+
+SELECT * FROM `tbProducts`;
+
+UPDATE `tbProducts`
+SET
+    `CREATED` = '2022-12-02';
