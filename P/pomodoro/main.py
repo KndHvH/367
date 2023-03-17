bc9f3889fbca6b318b725174346e940ec47c4ea0
knd <delend89@gmail.com>
2023-03-17 13:19:42 -0300

    pomodoro

---
+++ P/pomodoro/main.py
@@ -0,0 +1,39 @@
+import os
+import time
+
+
+def main():
+    small = [0.2,700] 
+    large = [2,400] 
+
+    #teste
+    play_sound(small[0],small[1])
+    time.sleep(1)
+    play_sound(small[0],small[1])
+    time.sleep(1)
+    play_sound(small[0],small[1])
+    time.sleep(1)
+    play_sound(large[0],large[1])
+    
+    while True:
+        for _ in range(4):
+            time.sleep(1500)
+            play_sound(small[0],small[1])
+            time.sleep(300)
+            play_sound(small[0],small[1])
+        play_sound(large[0],large[1])
+        time.sleep(1500)
+        play_sound(large[0],large[1])
+
+
+
+        
+    
+    
+
+def play_sound(duration,freq):
+    os.system(f'play -nq -t alsa synth {duration} sine {freq}')
+
+
+if __name__ == '__main__':
+   main()
