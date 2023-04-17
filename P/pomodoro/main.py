47914ed0968357e169c821654c4b11a0434ddeaf
knd <delend89@gmail.com>
2023-04-17 14:26:35 -0300

    pomodoro

--- P/pomodoro/main.py
+++ P/pomodoro/main.py
@@ -1,38 +1,38 @@
 import os
 import time
-
-
-# sudo apt install sox                                                                                                                         git:2*
+# sudo apt install sox
 
 
 def main():
     small = [0.2,700] 
     large = [2,400] 
-
-    #teste
-    play_sound(small[0],small[1])
-    time.sleep(1)
-    play_sound(small[0],small[1])
-    time.sleep(1)
-    play_sound(small[0],small[1])
-    time.sleep(1)
-    play_sound(large[0],large[1])
     
     while True:
         for _ in range(4):
-            time.sleep(1500)
             play_sound(small[0],small[1])
-            time.sleep(300)
+            intervalo(25,False)
+                
             play_sound(small[0],small[1])
+            intervalo(5)
+
         play_sound(large[0],large[1])
-        time.sleep(1500)
+        intervalo(25)
+
         play_sound(large[0],large[1])
 
 
 
-        
-    
-    
+
+
+
+def intervalo(minutos,trabalho = True):
+    for i in range(minutos):
+        display_text = 'Minutos' if i!=1 else 'Minuto'
+        display_text += ' para voltar ao trabalho.'if trabalho else ' para a pausa.'
+
+        print(f'Faltam {minutos - i} {display_text}')
+        time.sleep(60)
+
 
 def play_sound(duration,freq):
     os.system(f'play -nq -t alsa synth {duration} sine {freq}')
