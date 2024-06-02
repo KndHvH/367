1c9b4293535483db1f0f8d3239356100ad1a5c1f
KndHvH <delend89@gmail.com>
2024-06-02 17:54:59 -0300

    golang vars

--- ALURA/GO/01/hello.go
+++ ALURA/GO/01/hello.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
+
+func helloworld() {
+	fmt.Println("Hello, World!")
+}
+
+func var_helloworld() {
+	var nome string = "Matias"
+	var idade int = 22
+	fmt.Println("Hello, " + nome + "!")
+	fmt.Println(strconv.Itoa(idade) + " Anos!")
+	fmt.Println("Anos:", idade)
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	helloworld()
+	var_helloworld()
 }
