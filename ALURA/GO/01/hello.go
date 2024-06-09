066000366e1c59caa80b65d7fd7e3b8476f5d0f5
KndHvH <delend89@gmail.com>
2024-06-09 18:02:44 -0300

    go

--- ALURA/GO/01/hello.go
+++ ALURA/GO/01/hello.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"strconv"
+	"reflect"
 )
 
 func helloworld() {
@@ -12,9 +13,11 @@ func helloworld() {
 func var_helloworld() {
 	var nome string = "Matias"
 	var idade int = 22
+	numero := 10
 	fmt.Println("Hello, " + nome + "!")
 	fmt.Println(strconv.Itoa(idade) + " Anos!")
 	fmt.Println("Anos:", idade)
+	fmt.Println("Tipo da variável numero:", reflect.TypeOf(numero))
 }
 
 func main() {
